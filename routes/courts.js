e398cedc8e5ca70a2f8bdbea841f4a569018518c
Fétu Thimoté <lelemmingadorable@gmail.com>
2024-03-20 18:50:44 +0100

    Beginning courts frontend

--- routes/courts.js
+++ routes/courts.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-/* Get courts page. */
-router.get('/', (req, res) => {
-    res.render('court.hbs');
+/* GET courts page. */
+router.get("/", (req, res) => {
+  res.render("court.hbs");
+});
+
+/* GET booking court page. */
+router.get("/booking", (req, res) => {
+  res.render("court.hbs", { booking: true });
 });
 
 module.exports = router;
