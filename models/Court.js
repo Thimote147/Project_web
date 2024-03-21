6506ac5019f7f651cd0dff2e048c82d26f679b1e
Fétu Thimoté <lelemmingadorable@gmail.com>
2024-03-21 11:22:15 +0100

    Beginning the court details page

--- models/Court.js
+++ models/Court.js
@@ -1,1 +1,11 @@
 const db = require("./db_conf.js");
+
+module.exports.all = () => {
+    const stmt = db.prepare("SELECT id, name, flooring FROM courts");
+    return stmt.all();
+};
+
+module.exports.findById = (id) => {
+    const stmt = db.prepare("SELECT * FROM courts WHERE id = ?");
+    return stmt.get(id);
+};
--- routes/courts.js
+++ routes/courts.js
@@ -1,14 +1,21 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const Court = require('../models/Court.js');
 
 /* GET courts page. */
-router.get("/", (req, res) => {
-  res.render("court.hbs");
+router.get('/', (req, res) => {
+  res.render('court.hbs', { courts: Court.all() });
+});
+
+/* GET details court page. */
+router.get('/details', (req, res) => {
+  console.log(Court.findById(req.query.id));
+  res.render('court.hbs', { courts: Court.all(), court: Court.findById(req.query.id) })
 });
 
 /* GET booking court page. */
-router.get("/booking", (req, res) => {
-  res.render("court.hbs", { booking: true });
+router.get('/booking', (req, res) => {
+  res.render('court.hbs', { booking: true });
 });
 
 module.exports = router;
