498e761aa34b8a550164a294a7c803364f9a1e2a
Alexandre01Dev <34719450+Alexandre01Dev@users.noreply.github.com>
2024-03-26 10:35:47 +0100

    Coach update

--- models/Coach.js
+++ models/Coach.js
@@ -1,1 +1,6 @@
 const db = require("./db_conf.js");
+
+module.exports.all = () => {
+    const stmt = db.prepare("SELECT id,firstname,surname, email FROM users WHERE is_coach = 1");
+    return stmt.all();
+};
--- routes/coaches.js
+++ routes/coaches.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
+const Coach = require('../models/Coach.js')
 /* Get coaches page. */
 router.get('/', (req, res) => {
-    res.render('coach.hbs');
+    res.render('coach.hbs', { coaches: Coach.all() });
+});
+router.get('/bio', (req, res) => {
+
+    let coach = Coach.all();
+    res.render('coach.hbs', { coaches: coach, coach: user});
 });
 
 module.exports = router;
